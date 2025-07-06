4b7ac4aede3a975fdec5f1f7bce29b413b0b8d16
Austriana <Marco.Mang@gmx.de>
2025-07-06 20:03:06 +0200

    menu display changes

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ let menuImage = document.getElementById("menuImage");
 menuImage.addEventListener("click", () => {
     menu();
 })
+let menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener('click', () => {
+    menu();
+})
+
 function menu(){
     let menuFenster = document.getElementById("menuFenster").style;
     if(menuFenster.display === "inline-block"){
         menuFenster.display = "none";
-    } else menuFenster.display = "inline-block";
+        menuImage.style.display = "inline-block";
+    } else {
+        menuFenster.display = "inline-block"
+        menuImage.style.display = "none";
+    };
 };
