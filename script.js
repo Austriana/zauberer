a713c5a68d0ec90974ba8b68bfa2a7b11bc4cd89
Austriana <Marco.Mang@gmx.de>
2025-07-07 10:59:41 +0200

    added Site Menu

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ let menuBtn = document.getElementById('menuBtn');
 menuBtn.addEventListener('click', () => {
     menu();
 })
+let siteMenuImage = document.getElementById("siteMenuImage");
+siteMenuImage.addEventListener("click", () => {
+    siteMenu();
+})
+let siteMenuHome = document.getElementById("siteMenuHome");
+siteMenuHome.addEventListener("click", () => {
+    siteMenu();
+})
+let siteMenuLeistungen = document.getElementById("siteMenuLeistungen");
+siteMenuLeistungen.addEventListener("click", () => {
+    siteMenu();
+})
+let siteMenuTeam = document.getElementById("siteMenuTeam");
+siteMenuTeam.addEventListener("click", () => {
+    siteMenu();
+})
+let siteMenuKontakt = document.getElementById("siteMenuKontakt");
+siteMenuKontakt.addEventListener("click", () => {
+    siteMenu();
+})
+
 
 function menu(){
     let menuFenster = document.getElementById("menuFenster").style;
@@ -17,3 +38,18 @@ function menu(){
         menuImage.style.display = "none";
     };
 };
+
+function siteMenu(){
+    let headerContainer = document.getElementById('headerContainer').style;
+    let mainContainer = document.getElementById('mainContainer').style;
+    let siteMenuContainer = document.getElementById('siteMenuContainer').style
+    if (mainContainer.display === 'none') {
+        mainContainer.display = 'inline-block';
+        headerContainer.display = "block"
+        siteMenuContainer.display = 'none'
+    } else {
+        mainContainer.display = 'none';
+        headerContainer.display = "none";
+        siteMenuContainer.display = 'block'
+    }
+}
