865eda71f71030dd70a7d51add56a6f2ce43f0be
Austriana <Marco.Mang@gmx.de>
2025-07-23 00:02:48 +0200

    kontakt add prompt etc

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ siteMenuGeschäftsKunden.addEventListener("click", () => {
     burgerContainer.classList.toggle('active');
     siteMenu();
 });
-// let senden = document.getElementById('senden');
-// senden.addEventListener('click', () => {
-//     sendEmail();
-// });
 
 function siteMenu(){
     let siteMenuContainer = document.getElementById('siteMenuContainer')
@@ -56,21 +52,4 @@ function siteMenu(){
         siteMenuContainer.classList.toggle('active');
         siteMenuContainer.classList.remove('deactive');
     }
-};
-
-function sendEmail(){
-    let vorName = document.getElementById('vorName').value;
-    let nachName = document.getElementById('nachName').value;
-    let email = document.getElementById('email').value;
-    let telefon = document.getElementById('telefon').value;
-    let nachricht = document.getElementById('nachricht').value;
-    if(vorName !== '' && nachName !== '' && email !== '' && nachricht !== ''){
-        alert(` Hallo ${vorName} ${nachName},\n
-                Email: ${email}\n
-                Nachricht: ${nachricht}\n                
-                Telefon Nummer: ${telefon||'-'}\n
-                Vielen Dank! 💜`);
-    } else{
-        alert("Bitte alle Pflichtfelder ausfüllen damit wir uns besser vorbereiten können! Vielen Dank 💜");
-    };
 };
---
+++ send.js
@@ -0,0 +1,21 @@
+let senden = document.getElementById('senden');
+senden.addEventListener('click', () => {
+    sendEmail();
+});
+
+function sendEmail(){
+    let vorName = document.getElementById('vorName').value;
+    let nachName = document.getElementById('nachName').value;
+    let email = document.getElementById('email').value;
+    let telefon = document.getElementById('telefon').value;
+    let nachricht = document.getElementById('nachricht').value;
+    if(vorName !== '' && nachName !== '' && email !== '' && nachricht !== ''){
+        alert(` Hallo ${vorName} ${nachName},\n
+                Email: ${email}\n
+                Nachricht: ${nachricht}\n                
+                Telefon Nummer: ${telefon||'-'}\n
+                Vielen Dank! 💜`);
+    } else{
+        alert("Bitte alle Pflichtfelder ausfüllen damit wir uns besser vorbereiten können! Vielen Dank 💜");
+    };
+};
